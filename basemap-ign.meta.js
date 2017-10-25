3060bc32accceb1afc37de2fc9e55fa487aa05b9
Ciriol <ciriol.noneb@gmail.com>
2017-10-25 13:21:41 +0200

    Create basemap-ign.meta.js

---
+++ basemap-ign.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           IITC plugin: IGN map tiles
+// @id             iitc-plugin-basemap-ign
+// @category       Map Tiles
+// @version        0.1
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://static.fonk.me/plugins/iitc-ign.meta.js
+// @downloadURL    https://github.com/Ciriol/iitc_ign_layer/raw/master/basemap-ign.user.js
+// @description    Add the IGN map tiles as an optional layer.
+// @include        https://ingress.com/intel*
+// @include        http://ingress.com/intel*
+// @match          https://ingress.com/intel*
+// @match          http://ingress.com/intel*
+// @include        https://ingress.com/mission/*
+// @include        http://ingress.com/mission/*
+// @match          https://ingress.com/mission/*
+// @match          http://ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
