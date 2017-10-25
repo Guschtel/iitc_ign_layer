b287063cffd4045df0c7524967efe5121fb0bb55
Ciriol <ciriol.noneb@gmail.com>
2017-10-25 13:22:34 +0200

    Update basemap-ign.user.js

--- basemap-ign.user.js
+++ basemap-ign.user.js
@@ -2,7 +2,7 @@
 // @name           IITC plugin: IGN map tiles
 // @id             iitc-plugin-basemap-ign
 // @category       Map Tiles
-// @version        0.1
+// @version        0.2
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Ciriol/iitc_ign_layer/raw/master/basemap-ign.meta.js
 // @downloadURL    https://github.com/Ciriol/iitc_ign_layer/raw/master/basemap-ign.user.js
