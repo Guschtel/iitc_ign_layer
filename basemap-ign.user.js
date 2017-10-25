2be5ee862f7184785218149a99c10642877a3593
Ciriol <ciriol.noneb@gmail.com>
2017-10-25 13:12:31 +0200

    Update basemap-ign.user.js

--- basemap-ign.user.js
+++ basemap-ign.user.js
@@ -5,7 +5,7 @@
 // @version        0.1
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://static.fonk.me/plugins/iitc-ign.meta.js
-// @downloadURL    https://static.fonk.me/plugins/iitc-ign.user.js
+// @downloadURL    https://github.com/Ciriol/iitc_ign_layer/raw/master/basemap-ign.user.js
 // @description    Add the IGN map tiles as an optional layer.
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
