4af5cfc7bd2a02a00efdce506b08320247cd9048
Ciriol <ciriol.noneb@gmail.com>
2018-10-30 15:59:31 +0100

    Update basemap-ign.meta.js
    change on the intel web site

--- basemap-ign.meta.js
+++ basemap-ign.meta.js
@@ -2,18 +2,18 @@
 // @name           IITC plugin: IGN map tiles
 // @id             iitc-plugin-basemap-ign
 // @category       Map Tiles
-// @version        0.3
+// @version        0.4
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Ciriol/iitc_ign_layer/raw/master/basemap-ign.meta.js
 // @downloadURL    https://github.com/Ciriol/iitc_ign_layer/raw/master/basemap-ign.user.js
 // @description    Add the IGN map tiles as an optional layer.
-// @include        https://www.ingress.com/intel*
-// @include        https://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        https://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
+// @include        https://intel.ingress.com/intel*
+// @include        https://intel.ingress.com/intel*
+// @match          https://intel.ingress.com/intel*
+// @match          https://intel.ingress.com/intel*
+// @include        https://intel.ingress.com/mission/*
+// @include        https://intel.ingress.com/mission/*
+// @match          https://intel.ingress.com/mission/*
+// @match          https://intel.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
