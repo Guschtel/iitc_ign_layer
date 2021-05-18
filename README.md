51116d92ca5e7cf459209f8c5da491b63f40d44b
Guschtel <github@guschtel.de>
2021-05-18 21:23:53 +0200

    Fix README.md and Metadata

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # iitc_ign_layer
-IITC Plugin for IGN  Layer basemap
+IITC Plugin for IGN Layer basemap
 
-IITC : https://iitc.me/
+IITC : https://iitc.app/
 
 IGN : https://www.geoportail.gouv.fr/donnees/carte-ign
+
+Code modified by Guschtel
+
+Original code by https://github.com/Ciriol, to be found at https://github.com/Ciriol/iitc_ign_layer
--- basemap-ign.meta.js
+++ basemap-ign.meta.js
@@ -2,11 +2,11 @@
 // @name           IITC plugin: IGN map tiles
 // @id             iitc-plugin-basemap-ign
 // @category       Map Tiles
-// @version        0.4
+// @version        0.5
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://github.com/Ciriol/iitc_ign_layer/raw/master/basemap-ign.meta.js
-// @downloadURL    https://github.com/Ciriol/iitc_ign_layer/raw/master/basemap-ign.user.js
-// @description    Add the IGN map tiles as an optional layer.
+// @updateURL      https://github.com/Guschtel/iitc_ign_layer/raw/master/basemap-ign.meta.js
+// @downloadURL    https://github.com/Guschtel/iitc_ign_layer/raw/master/basemap-ign.user.js
+// @description    Add the IGN map tiles as an optional layer. Modified by @Guschtel Original Code from https://github.com/Ciriol/iitc_ign_layer
 // @include        https://intel.ingress.com/intel*
 // @include        https://intel.ingress.com/intel*
 // @match          https://intel.ingress.com/intel*
