632e3c6b4c8d894cebd241a38da5debfdac45f5b
guschtel <github@guschtel.de>
2024-03-23 14:51:56 +0100

    Adjust match'es for current intel map

--- basemap-ign.meta.js
+++ basemap-ign.meta.js
@@ -7,13 +7,7 @@
 // @updateURL      https://github.com/Guschtel/iitc_ign_layer/raw/master/basemap-ign.meta.js
 // @downloadURL    https://github.com/Guschtel/iitc_ign_layer/raw/master/basemap-ign.user.js
 // @description    Add the IGN map tiles as an optional layer. Modified by @Guschtel Original Code from https://github.com/Ciriol/iitc_ign_layer
-// @include        https://intel.ingress.com/intel*
-// @include        https://intel.ingress.com/intel*
-// @match          https://intel.ingress.com/intel*
-// @match          https://intel.ingress.com/intel*
-// @include        https://intel.ingress.com/mission/*
-// @include        https://intel.ingress.com/mission/*
-// @match          https://intel.ingress.com/mission/*
-// @match          https://intel.ingress.com/mission/*
+// @match           *://intel.ingress.com/*
+// @match           *://intel-x.ingress.com/*
 // @grant          none
 // ==/UserScript==
